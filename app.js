8d62cdd3c7810a19e2edbbeacbc4b7e4ce694ff9
Jaciel Aldair Pérez Martinez <73447465+JacielAldairPerezMartinez@users.noreply.github.com>
2022-07-05 20:48:56 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("Procesamiento de imagenes");
+document.write('Procesamiento de imagenes');
+
+document.write('</br> Este es el proyecto de procesamiento de imagenes :3');
---
+++ app.ts
@@ -0,0 +1,1 @@
+console.log("Procesamiento de imagenes");
